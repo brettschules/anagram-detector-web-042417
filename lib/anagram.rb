5bb215616ed0c652bee33038dcbfc200c50dff92
Brett Schules <brettschules@gmail.com>
2017-04-30 09:52:25 -0400

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    word_check = @word.split("").sort
+    words_array.select { |word| @word.split("").sort == word.split("").sort }
+  end
+end
